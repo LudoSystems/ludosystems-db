426e465ac96842389b4a46057d760ad4b23712e7
Abbie Schenk <abbieschenk@gmail.com>
2021-04-06 11:06:47 -0600

    Split db_init into two files because it wasn't working

---
+++ db_init-2.sql
@@ -0,0 +1,4 @@
+-- /c to ludobaum and run this
+CREATE SCHEMA ludobaum AUTHORIZATION ludobaum;
+
+GRANT ALL PRIVILEGES ON DATABASE ludobaum TO ludobaum;
--- db_init.sql
+++ db_init.sql
@@ -1,11 +1,4 @@
--- database setup
-
+-- database setup on postgres connection
 CREATE USER ludobaum;
 
 CREATE DATABASE ludobaum WITH OWNER ludobaum;
-
-\c ludobaum;
-
-CREATE SCHEMA ludobaum AUTHORIZATION ludobaum;
-
-GRANT ALL PRIVILEGES ON DATABASE ludobaum TO ludobaum
