3a123118603d4ff2d3bfea62022476c88af3c91f
Abbie Schenk <abbieschenk@gmail.com>
2021-06-09 17:11:29 -0600

    Added note to add a password to the ludosystems user

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,6 @@
 -- database setup on postgres connection
+
+-- use CREATE USER ludosystems WITH PASSWORD '...' if using this anywhere except development!
 CREATE USER ludosystems;
 
 CREATE DATABASE ludosystems WITH OWNER ludosystems;
